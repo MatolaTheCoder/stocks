40157031858071469bf3d9c7025382aff513ef47
MatolaTheCoder <isacmatolajr6@gmail.com>
2024-01-26 22:51:22 +0300

    views(vote view)

--- meusite/polls/views.py
+++ meusite/polls/views.py
@@ -12,5 +12,5 @@ def results(request, question_id):
     return HttpResponse(response % question_id)
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s.")
+    return HttpResponse("You're voting on question %s."%question_id)
 
